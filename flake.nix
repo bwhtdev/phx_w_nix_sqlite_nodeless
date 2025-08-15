3420b25f7ce0773f51eda1a86eea61d30801d18f
bwhtdev <bradscottwhite@gmail.com>
2025-08-14 21:50:03 -0400

    created flake.nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Elixir Phoenix Dev Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            erlang
+            elixir
+            sqlite3
+            tailwindcss
+          ];
+          shellHook = ''
+            export ERL_AFLAGS="-kernel shell_history enabled"
+          '';
+        };
+      });
+}
